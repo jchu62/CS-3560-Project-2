1a958644016768d484f5bb01774f178c679d4c45
Jesse Chu <jwchu@cpp.edu>
2022-11-10 16:05:57 -0800

    Attempt to get UI to work again (failure)

--- src/main/java/com/example/project2/Admin.java
+++ src/main/java/com/example/project2/Admin.java
@@ -5,7 +5,6 @@ import javax.swing.border.EmptyBorder;
 
 public class Admin extends JFrame{
 
-    private JPanel treePanel;
     private JTree tree;
     private JPanel mainPanel;
     private JTextField textField;
@@ -29,7 +28,6 @@ public class Admin extends JFrame{
     private Admin()
     {
         this.setTitle("Admin View");
-        treePanel = new JPanel();
         tree = new JTree();
         mainPanel = new JPanel();
         textField = new JTextField();
@@ -41,9 +39,36 @@ public class Admin extends JFrame{
         userTotalMessageButton = new JButton();
         positiveMessagePercentButton = new JButton();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 640, 480);
-
-        mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+//        setBounds(360, 120, 640, 480);
+//
+//        mainPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setSize(640, 480);
         mainPanel.setLayout(null);
+
+        // will wait on implementing tree
+
+        textField.setBounds(40,40,100,50);
+        mainPanel.add(textField);
+
+        addUserButton.setBounds(500, 50, 200, 200);
+        mainPanel.add(addUserButton);
+
+        addGroupButton.setBounds(500, 60, 200, 200);
+        mainPanel.add(addGroupButton);
+
+        openUserButton.setBounds(300, 300, 200, 200);
+        mainPanel.add(openUserButton);
+
+        userTotalButton.setBounds(200, 200, 100, 100);
+        mainPanel.add(userTotalButton);
+
+        groupTotalButton.setBounds(10,10,30,30);
+        mainPanel.add(groupTotalButton);
+
+        userTotalMessageButton.setBounds(40,40, 40,40);
+        mainPanel.add(userTotalMessageButton);
+
+        positiveMessagePercentButton.setBounds(4,4,4,4);
+        mainPanel.add(positiveMessagePercentButton);
     }
 }
